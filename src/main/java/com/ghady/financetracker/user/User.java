016c70b630a9a0f429da401710927539021683ee
ghadiz <geeziadeh@gmail.com>
2026-02-22 17:18:57 +0000

    add User entity

---
+++ src/main/java/com/ghady/financetracker/user/User.java
@@ -0,0 +1,33 @@
+package com.ghady.financetracker.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
+}
