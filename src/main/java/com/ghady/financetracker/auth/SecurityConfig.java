9f87b608dcaf48021d9e3fd458f8906a6a447bc9
ghadiz <geeziadeh@gmail.com>
2026-03-06 02:21:37 +0200

    update frontend url

--- src/main/java/com/ghady/financetracker/auth/SecurityConfig.java
+++ src/main/java/com/ghady/financetracker/auth/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://finance-frontend-indol.vercel.app/"));
+                "https://finance-frontend-jade.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
